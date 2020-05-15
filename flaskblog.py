92596cbae846c21dd0eaa4cf96b11b24a03ee587
Anurag Kumar <anuragk1991@gmail.com>
2020-05-15 13:13:46 +0530

    db init and layout changes

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegisterForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '879ec7089103eb97c6a166b64e358ea8e8b295fb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(100), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+	image_file = db.Column(db.String(60), nullable=False, default='default.jpg')
+	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def __repr__(self):
+		return f'User({self.email}, {self.username}, {self.image_file})'
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
 
 posts = [
 	{
@@ -33,11 +55,21 @@ def about():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	form = RegisterForm()
-	return render_template('register.html', form=form)
+	if form.validate_on_submit():
+		flash('Account Created For {}'.format(form.username.data), 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'anuragk1991@gmail.com' and form.password.data == '123456':
+			flash('Logged In!', 'success')
+			return redirect('home')
+		else:
+			flash('Invalid email or password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
